037dce230e95c361c7dc80ee1d6d70f66230b9fd
Diva <divasriv02@gmail.com>
2023-01-17 18:02:12 +0530

    Iteration #1 pangram

---
+++ pangram/pangram.py
@@ -0,0 +1,6 @@
+def is_pangram(sentence):
+    alphabets='abcdefghijklmnopqrstuvwxyz'
+    for letter in alphabets:
+        if letter not in sentence.lower():
+            return False
+    return True
